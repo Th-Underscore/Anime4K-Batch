54f32a87406b1e8ddec761a471dc4e63ad9bad4e
Th-Underscore <therealunderscore16@gmail.com>
2025-07-26 04:21:06 -0400

    fix: Remove duplicate args

--- scripts/utils/prompt.cmd
+++ scripts/utils/prompt.cmd
@@ -4,6 +4,14 @@ set "SCRIPT=%~1"
 set "INPUT=%~2"
 shift & shift
 
+set "params="
+:collect
+if "%1"=="" goto :main
+shift
+set "params=%params% %1"
+goto :collect
+
+:main
 set /p "args=Args (optional, remember double quotes if necessary): "
 
-call "%SCRIPT%" "%INPUT%" %* %args%
+call "%SCRIPT%" "%INPUT%" %params% %args%
