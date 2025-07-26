ad1cb754f2e3b0d62d7d8be4c69f55cff8adffb9
Th-Underscore <therealunderscore16@gmail.com>
2025-07-26 04:33:53 -0400

    fix: My stupidity

--- scripts/utils/prompt.cmd
+++ scripts/utils/prompt.cmd
@@ -1,17 +1,5 @@
 @echo off
 
-set "SCRIPT=%~1"
-set "INPUT=%~2"
-shift & shift
-
-set "params="
-:collect
-if "%1"=="" goto :main
-shift
-set "params=%params% %1"
-goto :collect
-
-:main
 set /p "args=Args (optional, remember double quotes if necessary): "
 
-call "%SCRIPT%" "%INPUT%" %params% %args%
+call %* %args%
